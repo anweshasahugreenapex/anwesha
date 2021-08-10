d87ce8ae4c511e1b8f8a3d21367b20b77a7a80b4
anweshasahugreenapex <anwesha.sahu@green-apex.com>
2021-08-10 06:03:19 -0700

    updated task1

--- task1.js
+++ task1.js
@@ -4,29 +4,29 @@ let a = prompt("enter the 1st number(0-9)");
 let b = prompt("enter the 2nd number(12-25)");//input 2nd number using prompt
 
 
-    let  addition= Number(a) + Number(b); //addition
+    const  addition= Number(a) + Number(b); //addition
     document.write("Adition  : ", addition + "<br>");
     
-    let subtraction = Number(a) - Number(b); //subtraction
+    const subtraction = Number(a) - Number(b); //subtraction
     document.write("Subtraction  : ", subtraction + "<br>");
     
-    let multiplication = Number(a) * Number(b); //multiplication
+    const multiplication = Number(a) * Number(b); //multiplication
     document.write("Multiplication  :", multiplication + "<br>");
     
-    let division = (Number(a) / Number(b)).toFixed(2); // division of two numbers round of 2 digits after "."
+    const division = (Number(a) / Number(b)).toFixed(2); // division of two numbers round of 2 digits after "."
     
     document.write("Division  :", division + "<br>");
     
-    let reminder = Number(a) % Number(b); //reminder
+    const reminder = Number(a) % Number(b); //reminder
     document.write("Reminder  :", reminder + "<br>");
     
-    let exponentiation = Number(a) ** 5; //exponentiation
+    const exponentiation = Number(a) ** 5; //exponentiation
     document.write("Exponential power 5 of a :", exponentiation + "<br>");
     
-    let increment = Number(++a); //increment of digit
+    const increment = Number(++a); //increment of digit
     document.write("Incremented value : ", increment + "<br>");
     
-    let decrement = Number(--a); //decrement of digit
+    const decrement = Number(--a); //decrement of digit
     document.write("Decremented value : ", decrement + "<br");
     
 
