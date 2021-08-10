101d5c30c5dbb646eb16bd01b811c0b516f4825d
anweshasahugreenapex <anwesha.sahu@green-apex.com>
2021-08-10 05:21:44 -0700

    task4updated

--- task1.js
+++ task1.js
@@ -1,27 +1,32 @@
 let a = prompt("enter the 1st number(0-9)");
-let b = prompt("enter the 2nd number(12-25)");
-let res;//result
-res = Number(a) + Number(b); //addition
-document.write("Adition  : ", res + "<br>");
+//input 1st number
 
-res = Number(a) - Number(b); //subtraction
-document.write("Subtraction  : ", res + "<br>");
+let b = prompt("enter the 2nd number(12-25)");//2nd number
 
-res = Number(a) * Number(b); //multiplication
-document.write("Multiplication  :", res + "<br>");
+    let  addition= Number(a) + Number(b); //addition
+    document.write("Adition  : ", addition + "<br>");
+    
+    let subtraction = Number(a) - Number(b); //subtraction
+    document.write("Subtraction  : ", subtraction + "<br>");
+    
+    let multiplication = Number(a) * Number(b); //multiplication
+    document.write("Multiplication  :", multiplication + "<br>");
+    
+    let division = (Number(a) / Number(b)).toFixed(2); // division of two numbers round of 2 digits after "."
+    
+    document.write("Division  :", division + "<br>");
+    
+    let reminder = Number(a) % Number(b); //reminder
+    document.write("Reminder  :", reminder + "<br>");
+    
+    let exponentiation = Number(a) ** 2; //exponentiation
+    document.write("Exponential  :", exponentiation + "<br>");
+    
+    let increment = Number(++a); //increment of digit
+    document.write("Incremented value : ", increment + "<br>");
+    
+    let decrement = Number(--a); //decrement of digit
+    document.write("Decremented value : ", decrement + "<br");
+    
 
-ret = (Number(a) / Number(b)).toFixed(2); // division of two numbers round of 2 digits after "."
 
-document.write("Division  :", res + "<br>");
-
-res = Number(a) % Number(b); //reminder
-document.write("Reminder  :", res + "<br>");
-
-res = Number(a) ** 2; //exponentiation
-document.write("Exponential  :", res + "<br>");
-
-res = Number(++a); //increment of digit
-document.write("Incremented value : ", res + "<br>");
-
-res = Number(--a); //decrement of digit
-document.write("Decremented value : ", res + "<br");
