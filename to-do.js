a1f9be1b61ce118e52a3eca74154b095b9ebde50
anweshasahugreenapex <anwesha.sahu@green-apex.com>
2021-08-14 09:21:59 -0700

    updated the to-do list task with css

--- to-do.js
+++ to-do.js
@@ -2,7 +2,7 @@ let list = []; //array to store the user entered tasks
 let inp = document.getElementById("inp"); //fetch the input enters in text input box
 let todolist = document.getElementById("todolist");
 
-document.getElementById("btn").addEventListener("click", ()=> {
+document.getElementById("btn").addEventListener("click", () => {
   //function to run when add button is clicked
 
   if (document.querySelector("#newtask input").value.length == 0) {
@@ -17,74 +17,38 @@ document.getElementById("btn").addEventListener("click", ()=> {
   document.querySelector("#newtask input").value = "";
 });
 
-
-
-let showlist=()=> {
+let showlist = () => {
   //function to display the task with delete and update button
   todolist.innerHTML = "";
-  list.map(function (n, i) {//map
-    if(n!=""){
+  list.map(function (n, i) {
+    //map
+    if (n != "") {
       todolist.innerHTML +=
-      "<li>" +
-      n +
-      "<button id='btn1' onclick='edititem("+i+")'>UPDATE</button>" +
-      "<button id='btn2' onclick='deleteitem(" +
-      i +
-      ")'>DELETE</button> </li>";
+        "<li>" +
+        n +
+        "<button id='btn1' onclick='edititem(" +
+        i +
+        ")'>UPDATE</button>" +
+        "<button id='btn2' onclick='deleteitem(" +
+        i +
+        ")'>DELETE</button> </li>";
     }
-    
-    
   });
-}
+};
 
-let deleteitem=(i)=> {
+let deleteitem = (i) => {
   //function to delete task
 
   list.splice(i, 1); //delete the task
   showlist();
-}
+};
 
-let edititem=(i)=>
- {
+let edititem = (i) => {
+  const indexx = i;
+  inp.value = list[indexx];
 
-const indexx=i;
-inp.value=(list[indexx])
+  list[indexx] = document.getElementById("btn").value;
+  // list[0]=list[indexx]
 
-list[indexx]=document.getElementById("btn").value
-// list[0]=list[indexx]
-
-showlist()
-
- 
- }
-
- 
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
+  showlist();
+};
