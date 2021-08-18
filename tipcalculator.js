0db0b9716c087b8cba5ccf7c25be049d41f285e7
anweshasahugreenapex <anwesha.sahu@green-apex.com>
2021-08-18 04:01:19 -0700

    added map function

--- tipcalculator.js
+++ tipcalculator.js
@@ -1,19 +1,19 @@
-let bills = [125, 555, 44]; //array to store the bill data
-let tips = new Array(); //array to store tips
-let totals = new Array(); //array to store total amount
+const bills = [125, 555, 44]; //array to store the bill data
+let tips = []; //array to store tips
+let totals = []; //array to store total amount
 
-let calculate=(bills)=> {
-  //function to calculate bills
-  for (let i = 0; i < bills.length; i++) {
-    tips[i] =
-      bills[i] <= 300 && bills[i] >= 50
-        ? (bills[i] * 15) / 100
-        : (bills[i] * 20) / 100;
-    totals[i] = tips[i] + bills[i];
-  }
-}
+const calculate = (bill) => {
+  //function to calculate tip
+  let tip = bill <= 300 && bill >= 50 ? (bill * 15) / 100 : (bill * 20) / 100;
+  return Number(tip);
+};
 
-calculate(bills);
-document.write("<br/>bills: " + bills); //print the bills array
-document.write("<br/>tips: " + tips); //print the tips array
-document.write("<br/>totals: " + totals); //print the totals array
+bills.map((item, index) => {
+  //map function to store  tips and total
+  tips[index] = calculate(item);
+  totals[index] = tips[index] + item;
+});
+
+document.write(`<br/>bills: ${bills}`); //print the bills array
+document.write(`<br/>tips:  ${tips}`); //print the tips array
+document.write(`<br/>totals:  ${totals}`); //print the totals array
