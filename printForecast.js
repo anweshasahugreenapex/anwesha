5e7d86920757fc38287ddac68a29920d3a577942
anweshasahugreenapex <anwesha.sahu@green-apex.com>
2021-08-17 22:14:28 -0700

    updated to es6 features

--- printForecast.js
+++ printForecast.js
@@ -1,17 +1,16 @@
 let data1 = [17, 21, 23]; //dataset 1
 let data2 = [12, 5, -5, 0, 4]; //dataset 2
-let arr = new Array();
+let result = "";
 
-let printForecast = (data1) => {
-  //function to print forecast
-
-  arr = data1;
-  let day = 1; //day variable set to 1 first for 1st day
-  arr.forEach(function (value) {
-    console.log(value + "ºC in " + day + " days "); //print on console
-    day++;
-  });
+let printForecast = (value) => {
+  value.map(
+    //map function to print the forecast
+    (dataval, day) => (result += `${dataval} ºC in ${day + 1} days<br/>`)
+  );
 };
 
-printForecast(data1); //call function printForecast
+printForecast(data1);
+
+document.getElementById("showresult").innerHTML = result; //call function printForecast
 printForecast(data2);
+document.getElementById("showresult").innerHTML = result; //call  function
