52518b9d9319fe4ee7e73aa956f0a47f1390d076
anweshasahugreenapex <anwesha.sahu@green-apex.com>
2021-08-21 01:25:51 -0700

    added restrictions to the input values

--- final.js
+++ final.js
@@ -7,28 +7,27 @@ const resetTable = () => {
   const confirmMessage = confirm(
     "Are you sure want to reset table data? It will delete all data from table. "
   );
- 
+
   if (confirmMessage) {
     document.getElementById("showResult").innerHTML = " ";
     document.getElementById("fname").value = "";
     document.getElementById("lname").value = "";
     document.getElementById("yr").value = "";
     document.getElementById("email").value = "";
-    index=null;//set index to null
-    indd=null;//set index id to null
-    data.length=0// make the data array empty.
+    index = null; //set index to null
+    indd = null; //set index id to null
+    data.length = 0; // make the data array empty.
     // alert("cleared Successfully");
   } else {
     alert("cancelled!");
   }
 };
-const resetinput=()=>{
-
+const resetinput = () => {
   document.getElementById("fname").value = "";
   document.getElementById("lname").value = "";
   document.getElementById("yr").value = "";
   document.getElementById("email").value = "";
-}
+};
 
 const addData = () => {
   //function to run on click of add button
@@ -39,41 +38,49 @@ const addData = () => {
     data[indd].lname = document.getElementById("lname").value;
     data[indd].age = document.getElementById("yr").value;
     data[indd].email = document.getElementById("email").value;
-    if (data[indd].fname != "" &&  data[indd].lname != "" && data[indd].age != "" && data[indd].email != ""){
+    if (
+      data[indd].fname != "" &&
+      data[indd].lname != "" &&
+      data[indd].age != "" &&
+      data[indd].email != ""
+    ) {
       print(data);
-    resetinput()//reset the input after updating
-    index=null//putting index value to null after updation
-
-    }
-    else {
+      resetinput(); //reset the input after updating
+      index = null; //putting index value to null after updation
+    } else {
       alert("All feilds are Mandatory..");
     }
-    
   } else {
     //input from user for new data
     const getfName = document.getElementById("fname").value;
     const getlName = document.getElementById("lname").value;
     const getage = document.getElementById("yr").value;
     const getEmail = document.getElementById("email").value;
-
-    if (getfName != "" && getlName != "" && getage != "" && getEmail != "") {
-      const obj = {
-        id: new Date().getUTCMilliseconds(),
-        fname: getfName,
-        lname: getlName,
-        age: getage,
-        email: getEmail,
-      };
-      data.push(obj);
-      print(data);
-      // $("#id").val("");
-      // $("#fname").val("");
-      // $("#lname").val("");
-      // $("#age").val("");
-      // $("#email").val("");
-      resetinput();
-    } else {
-      alert("All feilds are Mandatory..");
+    if (
+      validateEmail() &&
+      validatefirstname() &&
+      validatelastname() &&
+      validateage()
+    ) {
+      if (getfName != "" && getlName != "" && getage != "" && getEmail != "") {
+        const obj = {
+          id: new Date().getUTCMilliseconds(),
+          fname: getfName,
+          lname: getlName,
+          age: getage,
+          email: getEmail,
+        };
+        data.push(obj);
+        print(data);
+        // $("#id").val("");
+        // $("#fname").val("");
+        // $("#lname").val("");
+        // $("#age").val("");
+        // $("#email").val("");
+        resetinput();
+      } else {
+        alert("All feilds are Mandatory..");
+      }
     }
   }
 };
@@ -121,4 +128,60 @@ const updateRow = (delete_item) => {
   document.getElementById("lname").value = data[indd].lname;
   document.getElementById("yr").value = data[indd].age;
   document.getElementById("email").value = data[indd].email;
-}
+};
+
+const validateEmail = () => {
+  //function for email validation
+  let emailID = document.getElementById("email").value;
+  let format = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (emailID.match(format)) {
+    document.getElementById("email").focus();
+    return true;
+  }
+
+  alert("Please enter correct email ID");
+  document.getElementById("email").focus();
+  return false;
+};
+
+const validatefirstname = () => {
+  //function for firstname validation
+
+  const fnamee = document.getElementById("fname").value;
+  //  const lnamee=document.getElementById("lname").value;
+  if (fnamee != "" && isNaN(fnamee)) {
+    document.getElementById("fname").focus();
+    return true;
+  }
+  alert("Please enter valid first name");
+  document.getElementById("fname").focus();
+  return false;
+};
+
+const validatelastname = () => {
+  //function for last name validation
+
+  const lnamee = document.getElementById("lname").value;
+  if (lnamee != "" && isNaN(lnamee)) {
+    document.getElementById("lname").focus();
+    return true;
+  }
+  alert("Please enter valid last name");
+  document.getElementById("lname").focus();
+  return false;
+};
+
+const validateage = () => {
+  //function for age validation
+
+  const agee = document.getElementById("yr").value;
+  const formatage = /^[0-9]{1,2}$/;
+  if (agee.match(formatage)) {
+    document.getElementById("yr").focus();
+    return true;
+  }
+
+  alert("Please enter correct age");
+  document.getElementById("yr").focus();
+  return false;
+};
