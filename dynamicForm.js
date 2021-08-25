d921f4ff77b8818d54c9345a22af1182d304ec77
anweshasahugreenapex <anwesha.sahu@green-apex.com>
2021-08-24 23:29:27 -0700

    dynamic with css

--- dynamicForm.js
+++ dynamicForm.js
@@ -1,10 +1,10 @@
 // Fetching HTML Elements in Variables by ID.
-let x = document.getElementById("form_sample");
+let form_fetch = document.getElementById("form_sample");
 const createform = document.createElement("form"); // Create New Element Form
 createform.setAttribute("action", ""); // Setting Action Attribute on Form
 
 createform.setAttribute("method", "post"); // Setting Method Attribute on Form
-x.appendChild(createform);
+form_fetch.appendChild(createform);
 
 const heading = document.createElement("h2"); // Heading of Form
 heading.innerHTML = "Login Form ";
@@ -23,7 +23,7 @@ createform.appendChild(namelabel);
 const inputelement = document.createElement("input"); // Create Input Field for Name
 inputelement.setAttribute("type", "text");
 inputelement.setAttribute("id", "name");
-inputelement.setAttribute("name", "dname");
+
 createform.appendChild(inputelement);
 
 createform.appendChild(linebreak);
@@ -35,7 +35,7 @@ createform.appendChild(emaillabel);
 const emailelement = document.createElement("input"); // Create Input Field for E-mail
 emailelement.setAttribute("type", "text");
 emailelement.setAttribute("id", "email");
-emailelement.setAttribute("name", "demail");
+
 createform.appendChild(emailelement);
 
 const emailbreak = document.createElement("br");
@@ -48,7 +48,7 @@ createform.appendChild(passlabel);
 const passelement = document.createElement("input"); // Create Input Field for E-mail
 passelement.setAttribute("type", "text");
 passelement.setAttribute("id", "password");
-passelement.setAttribute("name", "dpassword");
+
 createform.appendChild(passelement);
 
 const passbreak = document.createElement("br");
@@ -56,12 +56,12 @@ createform.appendChild(passbreak);
 
 const submitelement = document.createElement("input"); // Append Submit Button
 submitelement.setAttribute("type", "submit");
-submitelement.setAttribute("name", "dsubmit");
+
 submitelement.setAttribute("value", "Submit");
 submitelement.setAttribute("id", "Submitbtn");
 createform.appendChild(submitelement);
 
-const ev = document.getElementById("Submitbtn");
+// const ev = document.getElementById("Submitbtn");
 
 Submitbtn.addEventListener("click", function () {//on click of submit button   alert submitted successfully.
   alert("submitted successfully");
