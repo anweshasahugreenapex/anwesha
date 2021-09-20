6f5a4f871c25c8c069d2ae5f87fa4b2096cda781
anweshasahugreenapex <anwesha.sahu@green-apex.com>
2021-09-20 19:10:01 -0400

    lifecycle

--- test/src/index.js
+++ test/src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//import Counter from "./Counter"; //for class based
+import Counter from "./Counter_usingFunction"; //for function based
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mount: false,
+      
+    };
+    this.setMount = () => this.setState({ mount: true });
+    this.setUnmount = () => this.setState({ mount: false });
+   
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.setMount}>mount Counter</button>
+        <button onClick={this.setUnmount}>unmount Counter</button>
+        
+        {this.state.mount && <Counter  />}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
