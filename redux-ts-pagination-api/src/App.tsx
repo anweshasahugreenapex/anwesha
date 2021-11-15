3016cde29e5dcd67c99d001920da352745e8e5d3
Anwesha <anwesha.sahu@green-apex.com>
2021-11-15 12:26:55 +0530

    completed

--- redux-ts-pagination-api/src/App.tsx
+++ redux-ts-pagination-api/src/App.tsx
@@ -2,12 +2,9 @@ import "./App.css";
 import { Provider } from "react-redux";
 import { store } from "./components/redux/store";
 import TableContainer from "./components/Table";
-import {BrowserRouter as Router,Route} from "react-router-dom"
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Switch } from "react-router-dom";
 import JsonData from "./components/JsonData";
-
-
-
 
 function App() {
   return (
@@ -15,11 +12,14 @@ function App() {
       <Provider store={store}>
         <Router>
           <Switch>
-            <Route  exact path="/" ><TableContainer/></Route>
-            <Route exact  path="/json/:title" ><JsonData/></Route>
-            </Switch>
+            <Route exact path="/">
+              <TableContainer />
+            </Route>
+            <Route exact path="/json/:title">
+              <JsonData />
+            </Route>
+          </Switch>
         </Router>
-     
       </Provider>
     </div>
   );
--- redux-ts-pagination-api/src/components/JsonData.tsx
+++ redux-ts-pagination-api/src/components/JsonData.tsx
@@ -1,8 +1,12 @@
-import { useLocation } from "react-router-dom";
+import { useLocation,useParams } from "react-router-dom";
 
 const JsonData = () => {
+    const title=useParams()
+    console.log("title")
   const location = useLocation();
+
   console.log("hello from jasonData");
+
   const jsondata = location.state;
   console.log(jsondata);
   const data = JSON.stringify(jsondata);
